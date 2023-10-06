9d18126a2a4ddc88aefff01e525b63a0d3f2ecc7
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-10-05 17:50:10 -0700

    solve task no 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,66 +2,37 @@
 """
 Compress web static package
 """
-from fabric import Connection
-from datetime import datetime
+from fabric.api import env, put, run
 import os
 
-
-env = {
-    'host_string': '100.25.19.204',
-    'user': 'ubuntu',
-    'key_filename': '/path/to/private_key.pem'
-}
+# Define the web server IP addresses
+env.hosts = ['<IP web-01>', '<IP web-02>']
 
 
 def do_deploy(archive_path):
-    """Deploys a web static package to a remote server."""
 
-    if not os.path.isfile(archive_path):
+    if not os.path.exists(archive_path):
         print("Archive file does not exist.")
         return False
 
     try:
-        # Establish a connection to the remote server
-        conn = Connection(env['host_string'],
-                          user=env['user'],
-                          connect_kwargs={'key_filename': env['key_filename']})
+        put(archive_path, "/tmp/")
 
-        # Get the current timestamp for the release folder
-        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_filename = os.path.basename(archive_path)
+        release_folder = "/data/web_static/releases/\
+{}".format(os.path.splitext(archive_filename)[0])
+        run("mkdir -p {}".format(release_folder))
+        run("tar -xzf /tmp/{} -C {}".format(archive_filename, release_folder))
+        run("rm /tmp/{}".format(archive_filename))
 
-        # Create the necessary directories
-        conn.run('mkdir -p /data/web_static/releases/web_static_{}/'
-                 .format(timestamp))
-        conn.run('mkdir -p /data/web_static/shared/')
+        run("mv {}/web_static/* {}/".format(release_folder, release_folder))
+        run("rm -rf {}/web_static".format(release_folder))
 
-        # Upload the archive to the temporary folder on the server
-        remote_archive_path = '/tmp/' + os.path.basename(archive_path)
-        conn.put(archive_path, remote=remote_archive_path)
+        run("rm -rf /data/web_static/current")
 
-        # Extract the archive into the release folder
-        conn.run('tar -xzf {} -C /data/web_static/releases/web_static_{}/'
-                 .format(remote_archive_path, timestamp))
+        run("ln -s {} /data/web_static/current".format(release_folder))
 
-        # Remove the temporary archive
-        conn.run('rm {}'.format(remote_archive_path))
-
-        # Move the contents of the extracted folder to the release folder
-        conn.run('mv /data/web_static/releases/web_static_{}/web_static/* \
-/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
-
-        # Remove the now empty 'web_static' folder
-        conn.run('rm -rf /data/web_static/releases/web_static_{}/web_static'
-                 .format(timestamp))
-
-        # Delete the old symbolic link if it exists
-        conn.run('rm -rf /data/web_static/current')
-
-        # Create a new symbolic link to the new release folder
-        conn.run('ln -s /data/web_static/releases/web_static_{}/ \
-/data/web_static/current'.format(timestamp))
-
-        print("Deployment successful.")
+        print("New version deployed!")
         return True
 
     except Exception as e:
